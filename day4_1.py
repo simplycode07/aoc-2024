e1cab21e1f9f6806112a465e25b99fa12545f14e
simplycode07 <simplycode07@gmail.com>
2024-12-04 11:29:03 +0530

    removed print statements

--- day4_1.py
+++ day4_1.py
@@ -8,23 +8,17 @@ for data in raw_data:
 word = "XMAS"
 
 def find_xmas(board, x, y, change):
-    # x = 0
-    # y = 0
-
     x += change[0]
     y += change[1]
     counter = 1
-    # change = (0, 1)
     while x < len(board[0]) and y < len(board) and counter < len(word) and x >= 0 and y >= 0:
         if board[y][x] == word[counter]:
-            print("found one", change, word[counter], counter, x, y)
             counter += 1
         else:
             break
 
         x += change[0]
         y += change[1]
-    # print("final counter: ", counter)
 
     return 1 if counter == len(word) else 0
 
@@ -36,13 +30,8 @@ for y in range(len(board)):
     for x in range(len(board[0])):
         if board[y][x] == "X":
             for change in allowed_changes:
-                # num_xmas += find_xmas(board, x, y, change)
-                if find_xmas(board, int(x), int(y), change):
-                    print(f"found {change} xmas {x}, {y}")
-                    num_xmas += 1
+                num_xmas += find_xmas(board, x, y, change)
 
-
-            # if find_xmas(board, x, y, (0, 1)):
 print(num_xmas)
 
 
