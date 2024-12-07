a4a487f0bc1d45c742e3d558d48309db7a4b1016
simplycode07 <simplycode07@gmail.com>
2024-12-07 11:13:10 +0530

    day 7 problem 2 (brute force, takes about 10 mins to run 😿)

---
+++ day7_2.py
@@ -0,0 +1,44 @@
+from itertools import product
+
+with open("input.txt") as file:
+    raw_data = file.readlines()
+
+equations = []
+
+for data in raw_data:
+    res = int(data.split(":")[0])
+    nums = [int(num) for num in data.split(":")[1].split()]
+
+    equations.append((res, nums))
+
+print(equations)
+
+def evaluate_exp(nums, operations):
+    res = nums[0]
+    for i in range(1, len(nums)):
+        if operations[i-1] == "+":
+            print("add:", res, nums[i], operations[i-1])
+            res += nums[i]
+        elif operations[i-1] == "*":
+            print("mul:", res, nums[i], operations[i-1])
+            res *= nums[i]
+        else:
+            res = int(str(res) + str(nums[i]))
+
+    return res
+
+result = 0
+operations = ["+", "*", "||"]
+
+for eqn in equations:
+    res, nums = eqn
+    # print(res, nums)
+    for ops in product(operations, repeat=len(nums) - 1):
+        print(ops)
+        if evaluate_exp(nums, ops) == res:
+            result += res
+            break
+        # else:
+        #     print(evaluate_exp(nums, ops), res)
+
+print(result)
