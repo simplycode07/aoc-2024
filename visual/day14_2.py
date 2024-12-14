c427eaf3c985fad06eb15222298740091c38e2b3
simplycode07 <simplycode07@gmail.com>
2024-12-14 21:12:35 +0530

    smoothened out the movement

--- visual/day14_2.py
+++ visual/day14_2.py
@@ -13,56 +13,67 @@ block_size = 8
 
 pattern = r"p=(-?\d+),(-?\d+)\s+v=(-?\d+),(-?\d+)"
 matches = re.findall(pattern, data)
-robots = [[int(x[0]), int(x[1]), int(x[2]), int(x[3])] for x in matches]
-
+robots = [[float(x[0]), float(x[1]), int(x[2]), int(x[3])] for x in matches]
 
 display = pygame.display.set_mode((width*block_size, height*block_size))
 
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
-# def update_robot(t):
-#     final_robots = []
-#     for start_x, start_y, vel_x, vel_y in robots:
-#         final_robots.append([(start_x + vel_x * t) % width, (start_y + vel_y * t) % height])
-#
-#     seen = set()
-#     for x, y in final_robots:
-#         if (x,y) in seen:
-#             return False
-#
-#         seen.add((x, y))
-#
-# for t in range(100000):
-#     worked = update_robot(t)
-#     print(worked, t)
+def draw_text(display, text):
+    text_surface = my_font.render(text, False, (255, 255, 255))
+    display.blit(text_surface, (0, 0))
 
+
+slowness = 600
 running = True
 count = 0
+paused = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-
-    seen = set()
-    unique = True
-    for robot in robots:
-        robot[0] += robot[2]
-        robot[1] += robot[3]
-
-        robot[0] %= width
-        robot[1] %= height
         
-        rect = pygame.Rect(robot[0] * block_size, robot[1] * block_size, block_size, block_size)
-        pygame.draw.rect(display, (13, 210, 240), rect)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                count += 100
+                for robot in robots:
+                    robot[0] += 100 * robot[2]
+                    robot[1] += 100 * robot[3]
 
-        if (robot[0], robot[1]) in seen:
-            unique = False
-        else: seen.add((robot[0], robot[1]))
+                    robot[0] %= width
+                    robot[1] %= height
+            if event.key == pygame.K_2:
+                count += 10
+                for robot in robots:
+                    robot[0] += 10 * robot[2]
+                    robot[1] += 10 * robot[3]
 
-    count += 1
+                    robot[0] %= width
+                    robot[1] %= height
+
+            if event.key == pygame.K_p:
+                paused = not paused
+
+    if int(count) == 6587:
+        paused = True
+        
+    if not paused:
+        for robot in robots:
+            robot[0] += (robot[2] / slowness)
+            robot[1] += (robot[3] / slowness)
+
+            robot[0] %= width
+            robot[1] %= height
+
+            rect = pygame.Rect(robot[0] * block_size, robot[1] * block_size, block_size, block_size)
+            pygame.draw.rect(display, (13, 210, 240), rect)
+
+
+        count += 1/slowness
+    draw_text(display, f"at: {count}")
     pygame.display.update()
     display.fill((30, 30, 20))
-    if unique: input(f"at: {count} Press Enter to Continue...")
 
     sleep(0.001)
 
